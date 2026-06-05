9089dc5e2e90225fe4804415b3f7e67e3f8162d5
o-az <23618431+o-az@users.noreply.github.com>
2026-06-05 01:15:07 -0700

    u

--- app/src/app/index.tsx
+++ app/src/app/index.tsx
@@ -1,14 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import * as z from 'zod'
-import {
-  ConsoleTemplate,
-  Input,
-  Panel,
-  PanelContent,
-  PanelHeader,
-  PanelTitle
-} from '@pipecat-ai/voice-ui-kit'
+import { ConsoleTemplate, Input } from '@pipecat-ai/voice-ui-kit'
 
 import '#app/style.css'
 
@@ -45,22 +38,18 @@ function VoiceConsole() {
     : {}
 
   return (
-    <main className='grid min-h-screen grid-rows-[auto_minmax(0,1fr)] gap-3 bg-background p-3'>
-      <Panel className='mx-auto w-full max-w-5xl'>
-        <PanelHeader variant='inline'>
-          <PanelTitle>Fish voice</PanelTitle>
-        </PanelHeader>
-        <PanelContent className='grid gap-2 p-3 pt-0'>
-          <Input
-            aria-label='Fish voice ID'
-            value={fishVoiceId}
-            onChange={event => setFishVoiceId(event.currentTarget.value)}
-            spellCheck={false}
-            autoComplete='off'
-            size='lg'
-          />
-        </PanelContent>
-      </Panel>
+    <main className='relative h-screen overflow-hidden bg-background'>
+      <div className='fixed left-2 top-2 z-50 w-[min(18rem,calc(100vw-4.75rem))]'>
+        <Input
+          aria-label='Fish voice ID'
+          value={fishVoiceId}
+          onChange={event => setFishVoiceId(event.currentTarget.value)}
+          spellCheck={false}
+          autoComplete='off'
+          placeholder='Fish voice ID'
+          size='sm'
+        />
+      </div>
 
       <ConsoleTemplate
         transportType='daily'
@@ -80,8 +69,6 @@ function VoiceConsole() {
         noUserVideo
         noScreenControl
         noBotVideo
-        noSessionInfo
-        collapseInfoPanel
         noTextRenderModeSwitch
         textRenderMode='karaoke'
         conversationElementProps={{
